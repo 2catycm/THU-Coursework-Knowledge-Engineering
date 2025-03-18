16cef3ecc5d563f676fcf01e4cceeee0df5c428a
叶璨铭 (Ye Canming) <ycm24@mails.tsinghua.edu.cn>
2025-03-18 22:57:51 +0800

    feat: 实现cnn

--- 4.新闻文本分类的作业/model/cnn.py
+++ 4.新闻文本分类的作业/model/cnn.py
@@ -1,29 +1,19 @@
-import numpy as np  # type: ignore
-import torch  # type: ignore
-from torch import nn  # type: ignore
+import numpy as np
+import torch
+from torch import nn
 
 
 class TextCNN(nn.Module):
     def __init__(
         self,
-        word_embeddings,
-        vector_size,
-        label2index,
-        pad_index,
-        filter_size=[2, 3, 4, 5],
-        channels=64,
-        max_length=1024,
-    ):
-        # Args:
-        #   word_embeddings: np.array of size N*D, containing pretrianed word2vec embedding
-        #   vector_size: int, word embedding dim
-        #   label2index: Dict
-        #   pad_index: int
-        #   filter_size: List[int], kernel size for each layer of CNN
-        #   channels: int, output channel for CNN
-        #   max_length: int
-        # Returns:
-        #   None
+        word_embeddings: np.ndarray,  # 预训练词向量矩阵(N*D)
+        vector_size: int,  # 词向量维度 D
+        label2index: dict,  # 标签到索引的映射
+        pad_index: int,  # 填充token的索引
+        filter_size: list[int] = [2, 3, 4, 5],  # CNN卷积核大小
+        channels: int = 64,  # CNN输出通道数
+        max_length: int = 1024,  # 最大序列长度
+    ) -> None:
         super(TextCNN, self).__init__()
         # Initialize embedding layer with pre-trained word_embeddings
         self.embedding = nn.Embedding.from_pretrained(
@@ -39,13 +29,31 @@ class TextCNN(nn.Module):
         # Final linear layer for label prediction; number of classes equals len(label2index)
         num_class = len(label2index)
         self.linear = nn.Linear(channels * len(filter_size), num_class)
+        self.max_length = max_length
 
-    def forward(self, inputs):
-        # Args:
-        #   inputs: torch.tensor of size N*L
-        # Returns:
-        #   predicted_logits: torch.tensor of size N*C (number of classes)
-        #############
-        # TODO
-        ##############
-        raise NotImplementedError
+    def forward(
+        self,
+        inputs: torch.Tensor,  # 输入张量(N*L)
+    ) -> torch.Tensor:  # 返回预测logits(N*K)， 不需要softmax
+        # check max_length
+        if inputs.size(1) > self.max_length:
+            inputs = inputs[:, : self.max_length]
+        # Embedding layer
+        x = self.embedding(inputs)  # 得到 (N*L*D)
+        # Convolutional layer
+        x = x.transpose(1, 2)  # 卷积需要将词向量维度放在最后 (N*D*L)
+        x = [conv(x) for conv in self.convs]
+        x = [nn.functional.gelu(i) for i in x]  # 每一个 i是 (N*C*Li) ， Li = L - ki + 1
+        # Pooling layer
+        x = [
+            nn.functional.max_pool1d(
+                i,
+                kernel_size=i.size(2),  # 对 Li 去做 max_pooling
+            ).squeeze(2)
+            for i in x  # 每一个 i是 (N*C*Li)
+        ]  # 每一个 item 变为 (N*C)
+        # Concatenate all pooling results
+        x = torch.cat(x, dim=1)  # 把每一个 item 拼接起来，变为 (N, C*len(filter_size))
+        # Linear layer
+        x = self.linear(x)  # 分类，得到 (N*K)
+        return x
