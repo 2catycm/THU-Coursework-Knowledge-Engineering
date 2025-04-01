4cc9b6ec886eeb9e1d389c4f43a19db06c7d9197
叶璨铭 (Ye Canming) <ycm24@mails.tsinghua.edu.cn>
2025-04-01 15:19:51 +0800

    feat: init

---
+++ 6.英文评论情感分类的作业/data/download.sh
@@ -0,0 +1,5 @@
+wget -O data.zip https://cloud.tsinghua.edu.cn/f/e688d8423424426c8787/?dl=1
+unzip data.zip
+mv A6-data/* ./
+rm -rf A6-data/
+rm -rf data.zip
---
+++ 6.英文评论情感分类的作业/model/cnn.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from torch import nn
+from allennlp.modules.elmo import Elmo
+
+
+class TextCNN(nn.Module):
+    def __init__(self, options_file, weight_file, vector_size, filter_size=[2,3,4,5], channels=64, max_length=1024):
+        # Args:
+        #   options_file: elmo file
+        #   weight_file: elmo weight file
+        #   vector_size: int, word embedding dim
+        #   filter_size: List[int], kernel size for each layer of CNN
+        #   channels: int, output channel for CNN
+        #   max_length: int
+        # Returns:
+        #   None
+        super(TextCNN, self).__init__()
+        self.embedding = Elmo(options_file, weight_file, 1, dropout=0)
+        ####################
+        # TODO
+        # 1.initialize embedding layer with word_embeddings
+        # 2.build a stack of 1-d CNNs with designated kernel size
+        # e.g. with filter_size=[2,3,4,5], 4 layers of CNN should be built and kernel size is set to 2,3,4,5, respectively.
+        # 3. the last linear layer for label prediction
+        #####################
+        raise NotImplementedError
+
+    def forward(self, inputs):
+        # Args:
+        #   inputs: torch.tensor of size N*L
+        # Returns:
+        #   predicted_logits: torch.tensor of size N*C (number of classes)
+        output = []
+        inputs = self.embedding(inputs)["elmo_representations"][0]
+        #############
+        # TODO
+        ##############
+        return output
