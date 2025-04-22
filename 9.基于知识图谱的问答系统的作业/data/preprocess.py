edd6d5b18fc17b0c9211e7b21bc6ed3e25a6257b
叶璨铭 <12011404@mail.sustech.edu.cn>
2025-04-22 16:34:47 +0800

    feat: ruff

--- 9.基于知识图谱的问答系统的作业/data/preprocess.py
+++ 9.基于知识图谱的问答系统的作业/data/preprocess.py
@@ -1,9 +1,10 @@
 import json
 
+
 def preprocess():
     kg = {}
-    with open('../zhishime.json', 'r', encoding='utf-8') as f:
-        #TODO
+    with open("../zhishime.json", "r", encoding="utf-8") as f:
+        # TODO
         raw_relation_data = list(map(json.loads, f))
 
     head2id = dict()
@@ -12,10 +13,9 @@ def preprocess():
     relation_triplets = []
 
     for item in raw_relation_data:
-        head = item['head']
-        tail = item['tail']
-        relation = item['relation']
-
+        head = item["head"]
+        tail = item["tail"]
+        relation = item["relation"]
 
         if head not in head2id:
             head2id[head] = len(head2id)
@@ -24,24 +24,23 @@ def preprocess():
         if relation not in relation2id:
             relation2id[relation] = len(relation2id)
 
-        relation_triplets.append((
-            head2id[head], relation2id[relation], tail2id[tail]
-        ))
+        relation_triplets.append((head2id[head], relation2id[relation], tail2id[tail]))
 
-    print(f'{len(head2id)} head entities, {len(tail2id)} tail entities, {len(relation2id)} relations.')
-    print(f'{len(relation_triplets)} relation triplets')
+    print(
+        f"{len(head2id)} head entities, {len(tail2id)} tail entities, {len(relation2id)} relations."
+    )
+    print(f"{len(relation_triplets)} relation triplets")
 
     kg = {
-        'head2id': head2id,
-        'tail2id': tail2id,
-        'relation2id': relation2id,
-        'relation_triplets': relation_triplets
+        "head2id": head2id,
+        "tail2id": tail2id,
+        "relation2id": relation2id,
+        "relation_triplets": relation_triplets,
     }
 
-
-
-    with open('./processed/kg.json', 'w', encoding='utf-8') as json_file:
+    with open("./processed/kg.json", "w", encoding="utf-8") as json_file:
         json.dump(kg, json_file, ensure_ascii=False, indent=4)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     preprocess()
